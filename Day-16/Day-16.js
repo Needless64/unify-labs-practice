c24a35864e6effbef28990121317f1dce9a7cfa9
Needless64 <lingamaditya3669@gmail.com>
2026-02-12 20:52:45 +0530

    Day-16

---
+++ Day-16/Day-16.js
@@ -0,0 +1,36 @@
+let name = "Adiya";
+let age = "19";
+let num = Number(age);
+
+let msg = `Hi i am ${name}, next i will be ${num + 1}`;
+
+let text = "Hello\nWorld";
+
+let pow = Math.pow(2, 3);
+let root = Math.sqrt(16);
+
+function greet(user = "Guest") {
+  return `Welcome ${user}`;
+}
+
+let sum = (a, b) => a + b;
+
+function testScope() {
+  let x = 10;
+  if (x > 5) {
+    let y = 20;
+    return y;
+  }
+}
+
+let result1 = greet(name);
+let result2 = sum(5, 3);
+let result3 = testScope();
+
+console.log(msg);
+console.log(text);
+console.log(pow);
+console.log(root);
+console.log(result1);
+console.log(result2);
+console.log(result3);
